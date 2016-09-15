20760f245f3996e83e5a0e2b66b98e1acffcace1
Lily B <lilu459@mail.ru>
2016-09-15 08:24:37 +0300

    last one

--- includes/db/db_connection.php
+++ includes/db/db_connection.php
@@ -3,7 +3,9 @@ define("DB_SERVER", "localhost");
 define("DB_USER", "test_cms");
 define("DB_PASS", "test5740960");
 define("DB_NAME", "test_widget_corp");
-//1. Create a database connection
+
+
+//Create a database connection
 
 $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 
